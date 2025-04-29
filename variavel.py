011ea364953a900352ee995fe24cd19e686fe391
Érica Luana <ericaguedes.00@gmail.com>
2025-04-29 19:39:33 -0300

    Update variavel.py

--- variavel.py
+++ variavel.py
@@ -1,2 +1,13 @@
 nome ="erica"
 print(nome)
+
+# CRIAÇÃO DE VARIAVEIS
+
+numero1 = 10
+numero2 = 15
+
+# soma
+soma = numero1 + numero2
+#print(soma)
+
+print(f"A soma entre {numero1} e {numero2} é igual a {soma}")
